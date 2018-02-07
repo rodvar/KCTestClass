4945267fe66636b3ebb833aa2f0d44ca09944fe8
Rodrigo Varela <rodvar@gmail.com>
2018-02-07 21:30:55 +1100

    * Custom application injecting main component

---
+++ app/src/main/java/com/rodvar/clasepruebakc/ClasePruebaKcApplication.kt
@@ -0,0 +1,34 @@
+package com.rodvar.clasepruebakc
+
+import android.app.Activity
+import android.app.Application
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import javax.inject.Inject
+
+/**
+ * Created by rodvar on 7/2/18.
+ */
+class ClasePruebaKcApplication : Application(), HasActivityInjector {
+    @Inject
+    lateinit var activityInjector: DispatchingAndroidInjector<Activity>
+
+    lateinit var component: AppComponent
+
+    override fun onCreate() {
+        super.onCreate()
+
+        this.createApplicationComponent()
+    }
+
+    /**
+     * Creates the main app component installing the main App Module
+     */
+    private fun createApplicationComponent() {
+        component = DaggerAppComponent.builder().appModule(AppModule(this)).build()
+        (component as AppComponent).inject(this)
+    }
+
+    override fun activityInjector(): AndroidInjector<Activity> = activityInjector
+}
